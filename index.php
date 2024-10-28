2e7406dc30753f61ec6715fa12b2b3f2b59c4035
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-28 14:22:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
   </head>
   
 <div>
-  <button>Calculate</button>
+  <input type="number" id="numinp" min="0">
+  <button onclick="calcFactorial()">Calculate</button>
 </div>
 <div>
   <button>Generate Number</button>
@@ -20,5 +21,21 @@
 <div>
   <button>Generate Random Hex Code</button>
 </div>
+
+<script>
+  function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n*factorial(n-1);
+  }
+
+  function calcFactorial() {
+    const input = document.getElementById('numinp').value;
+    const number = parseInt(input, 10);
+    
+  }
+</script>
+
   
 </html>
